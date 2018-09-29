e095092b708d2ea82d84fdf92697aaaeff234051
Ken Youens-Clark <kyclark@gmail.com>
2018-09-29 08:21:37 -0700

    worked on problem 2

--- homework/03/code/approx_expected_value.py
+++ homework/03/code/approx_expected_value.py
@@ -5,14 +5,7 @@
 # From A First Course in Machine Learning, Chapter 2.
 # Simon Rogers, 01/11/11 [simon.rogers@glasgow.ac.uk]
 # Approximating expected values via sampling
-import numpy
-# NOTE: If you are running on MacOSX and encounter an error like the following:
-#     RuntimeError: Python is not installed as a framework.
-#     The Mac OS X backend will not be able to function correctly
-#     if Python is not installed as a framework...
-# Then uncomment the following 2 lines:
-#import matplotlib
-#matplotlib.use('TkAgg')
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Turn off annoying matplotlib warning
@@ -21,10 +14,10 @@ import matplotlib.pyplot as plt
 
 
 # We are trying to estimate the expected value of
-# $f(y) = y^2$
+# $f(y) = 35 + 3x - 0.5(x^3) + 0.05(x^4)$
 ##
 # ... where
-# $p(y)=U(0,1)$
+# $p(y)=U(-4, 10)$
 ##
 # ... which is given by:
 # $\int y^2 p(y) dy$
@@ -45,10 +38,10 @@ font = {'family' : 'serif',
 
 
 def plot_fn(fn, a, b, fn_name, resolution=100):
-    x = numpy.append(numpy.array([a]),
-                     numpy.append(numpy.linspace(a, b, resolution), [b]))
+    x = np.append(np.array([a]),
+                     np.append(np.linspace(a, b, resolution), [b]))
 
-    # x = numpy.linspace(x_begin, x_end, resolution)
+    # x = np.linspace(x_begin, x_end, resolution)
     y = fn(x)
     y[0] = 0
     y[-1] = 0
@@ -68,7 +61,7 @@ def plot_fn(fn, a, b, fn_name, resolution=100):
 
 # Define the function y that we are going to plot
 def y_fn(x):
-    return numpy.power(x, 2)
+    return 35 + (3 * x) - (0.5 * np.power(x, 3)) + (0.05 * np.power(x, 4))
 
 
 def y_fn_name():
@@ -78,31 +71,31 @@ def y_fn_name():
     used by approx_expected_value
     :return: fname, x_tpos, y_tpos, year
     """
-    fname = r'$f(y) = y^2$'  # latex format of fn name
+    fname = r'$35 + 3x - 0.5x^3 + 0.05x^4$'  # latex format of fn name
     x_tpos = 0.1  # x position for plotting text of name
     y_tpos = 0.5  # y position for plotting text of name
     return fname, x_tpos, y_tpos
 
 
 # Plot the function!
-plot_fn(y_fn, 0, 1, y_fn_name)
+plot_fn(y_fn, -4, 10, y_fn_name)
 
 
 # Now we'll approximate the area under the curve using sampling...
 
 # Sample 1000 uniformly random values in [0..1]
-ys = numpy.random.uniform(low=0.0, high=1.0, size=1000)
+ys = np.random.uniform(low=0.0, high=1.0, size=1000)
 # compute the expectation of y, where y is the function that squares its input
-ey2 = numpy.mean(numpy.power(ys, 2))
+ey2 = np.mean(np.power(ys, 2))
 print('\nSample-based approximation: {:f}'.format(ey2))
 
 # Store the evolution of the approximation, every 10 samples
-sample_sizes = numpy.arange(1, ys.shape[0], 10)
-ey2_evol = numpy.zeros((sample_sizes.shape[0]))  # storage for the evolving estimate...
+sample_sizes = np.arange(1, ys.shape[0], 10)
+ey2_evol = np.zeros((sample_sizes.shape[0]))  # storage for the evolving estimate...
 # the following computes the mean of the sequence up to i, as i iterates
 # through the sequence, storing the mean in ey2_evol:
 for i in range(sample_sizes.shape[0]):
-    ey2_evol[i] = numpy.mean(numpy.power(ys[0:sample_sizes[i]], 2))
+    ey2_evol[i] = np.mean(np.power(ys[0:sample_sizes[i]], 2))
 
 # Create plot of evolution of the approximation
 plt.figure()
@@ -110,11 +103,11 @@ plt.figure()
 plt.plot(sample_sizes, ey2_evol)
 # The true, analytic result of the expected value of f(y)=y^2 where y ~ U(0,1): $\frac{1}{3}$
 # plot the analytic expected result as a red line:
-plt.plot(numpy.array([sample_sizes[0], sample_sizes[-1]]),
-         numpy.array([1./3, 1./3]), color='r')
+plt.plot(np.array([sample_sizes[0], sample_sizes[-1]]),
+         np.array([1./3, 1./3]), color='r')
 plt.xlabel('Sample size')
 plt.ylabel('Approximation of expectation')
-plt.title('Approximation of expectation of $f(y) = y^2$')
+plt.title('Approximation of expectation of $35 + 3x - 0.5x^3 + 0.05x^4$')
 plt.pause(.1)  # required on some systems so that rendering can happen
 
 plt.show()  # keeps the plot open
