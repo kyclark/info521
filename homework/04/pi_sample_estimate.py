891c8778c1884455fcda1309bf545b675281bf0c
Ken Youens-Clark <kyclark@gmail.com>
2018-10-25 20:55:50 -0700

    a start

--- homework/04/pi_sample_estimate.py
+++ homework/04/pi_sample_estimate.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 """
-A circle of radius R is inscribed in a square with sides 2R.
-The area of the circle is pi*R^2 and the area of the square is (2R)^2 or 4R^2.
+Author:  Ken Youens-Clark
+Date:    October 22, 2018
+Purpose:
+
+A circle of radius R is inscribed in a square with sides 2R. The area
+of the circle is pi*R^2 and the area of the square is (2R)^2 or 4R^2.
 Therefore the ratio of the areas is pi/4.
 
 To estimate pi, we will choose N samples from a uniform distribution
-in the radius of the circle (1). We can then use the Pythagorean theorem
-(a^2 + b^2 = c^2) (via the math.hypot function that computes Euclidean
-distance) to find the distance from the origin (0,0). If this
-is less than the radius squared (which is just one here), then the point
-falls within the circle. Multiple the number found to be within the circle
-by 4 and then divide by the number of samples to estimate pi.
+between 0 and the radius of the circle (1). We can then use the
+Pythagorean theorem (a^2 + b^2 = c^2) (via the math.hypot function
+that computes Euclidean distance) to find the distance from the origin
+(0,0). If this is less than the radius squared (which is just one
+here), then the point falls within the circle.  Because we are only
+looking at points in the upper-right quadrant of the circle, multiple
+the number found to be within the circle by 4 and then divide by the
+number of samples to estimate pi.
 
 Cf. https://en.wikipedia.org/wiki/Approximations_of_%CF%80
-
-Author: Ken Youens-Clark
-Date: October 22, 2018
 """
 
 import argparse
