8dd2fe23b89971f87c8c697e08cec84c8488ab9d
Ken Youens-Clark <kyclark@gmail.com>
2019-05-02 09:45:38 -0700

    final commits

---
+++ homework/hw-04-youens-clark/pi_sample_estimate.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+Author:  Ken Youens-Clark
+Date:    October 22, 2018
+Purpose:
+
+A circle of radius R is inscribed in a square with sides 2R. The area
+of the circle is pi*R^2 and the area of the square is (2R)^2 or 4R^2.
+Therefore the ratio of the areas is pi/4.
+
+To estimate pi, we will choose N samples from a uniform distribution
+between 0 and the radius of the circle (1). We can then use the
+Pythagorean theorem (a^2 + b^2 = c^2) (via the math.hypot function
+that computes Euclidean distance) to find the distance from the origin
+(0,0). If this is less than the radius squared (which is just one
+here), then the point falls within the circle.  Because the ratio
+of the area of the circle to the square is pi/4, multiply
+the number found to be within the circle by 4 and then divide by the
+number of samples to estimate pi.
+
+Cf. https://en.wikipedia.org/wiki/Approximations_of_%CF%80
+"""
+
+import argparse
+import sys
+from random import random
+from math import hypot
+
+
+# --------------------------------------------------
+def get_args():
+    """get args"""
+    parser = argparse.ArgumentParser(
+        description='Estimate pi',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        '-n',
+        '--num_samples',
+        help='Number of samples',
+        metavar='int',
+        type=int,
+        default='1000000')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+
+# --------------------------------------------------
+def die(msg='Something bad happened'):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    num_samples = args.num_samples
+
+    if num_samples < 1:
+        die('-n ({}) cannot be less than 1'.format(num_samples))
+
+    count = 0
+    for _ in range(0, num_samples):
+        x, y = random(), random()
+        if hypot(x, y) <= 1:
+            count += 1
+
+    print('pi ~ {:.06f}'.format(count * 4 / num_samples))
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
---
+++ homework/hw-04-youens-clark/plot_laplace_approx.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+"""
+Author : Ken Youens-Clark <kyclark@email.arizona.edu>
+Date   : 2018-10-25
+Purpose: Plot Laplace estimation of beta distribution
+"""
+
+import argparse
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import beta, norm
+
+
+# --------------------------------------------------
+def get_args():
+    """get args"""
+    parser = argparse.ArgumentParser(
+        description='Plot Laplace estimation of beta distribution',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        '-a',
+        '--alpha',
+        help='alpha value',
+        metavar='int',
+        type=int,
+        default=5)
+
+    parser.add_argument(
+        '-b', '--beta', help='beta value', metavar='int', type=int, default=5)
+
+    parser.add_argument(
+        '-n',
+        '--num_samples',
+        help='n value',
+        metavar='int',
+        type=int,
+        default=20)
+
+    parser.add_argument(
+        '-y', '--num_y', help='y value', metavar='int', type=int, default=10)
+
+    parser.add_argument(
+        '-o',
+        '--outfile',
+        help='Output file',
+        metavar='str',
+        type=str,
+        default=None)
+
+    parser.add_argument(
+        '-d', '--debug', help='Talk about (pop music)', action='store_true')
+
+    parser.add_argument(
+        '-N', '--no_viz', help='Do not show pictures', action='store_true')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+
+# --------------------------------------------------
+def die(msg='Something bad happened'):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
+
+
+# --------------------------------------------------
+def main():
+    """main"""
+    args = get_args()
+    alpha_val = args.alpha
+    beta_val = args.beta
+    n_val = args.num_samples
+    y_val = args.num_y
+    out_file = args.outfile
+
+    def debug(msg):
+        if args.debug:
+            warn(msg)
+
+    debug('$\alpha = {} \beta = {} N = {} y = {}$'.format(
+        alpha_val, beta_val, n_val, y_val))
+    r_hat = (1 - y_val - alpha_val) / (2 - alpha_val - n_val - beta_val)
+    debug('r_hat = {}'.format(r_hat))
+
+    t1 = (y_val + alpha_val - 1) / np.power(r_hat, 2)
+    t2 = (n_val - y_val + beta_val - 1) / np.power((1 - r_hat), 2)
+    d2 = (-1 * t1) - t2
+    sigma = -1 / d2
+    debug('d2 = {}'.format(d2))
+    debug('sigma = {}'.format(sigma))
+
+    beta_dist = beta(alpha_val, beta_val)
+    x = np.linspace(0, 1, 100)
+
+    plt.figure()
+    plt.plot(x, beta_dist.pdf(x), 'blue')
+
+    debug(norm.pdf(x, loc=r_hat, scale=np.sqrt(sigma)))
+    plt.plot(x, norm.pdf(x, loc=r_hat, scale=np.sqrt(sigma)), 'red')
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend(['Beta', 'Laplace'])
+
+    tmpl = r'Laplace approx for $\alpha$ = {}, $\beta$ = {}, N = {}, y = {}'
+    title = tmpl.format(alpha_val, beta_val, n_val, y_val)
+    plt.title(title)
+
+    if out_file:
+        ext = '.png'
+        if not out_file.endswith(ext):
+            out_file += ext
+
+        plt.savefig(out_file, fmt='png')
+
+    if not args.no_viz:
+        plt.show()
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
